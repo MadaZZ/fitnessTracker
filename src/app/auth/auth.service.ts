4ab0abd78bd51e77348dfb635800abf51d5ce1b2
MadaZZ <sparshranaazz@gmail.com>
2018-04-20 12:03:17 +0530

    Added dispatch state to authservice.
    Authservice now dispatches state for isLoading change

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Output } from '@angular/core';
+
 import { Router } from '@angular/router';
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
@@ -6,6 +7,10 @@ import { Subject } from 'rxjs/Subject';
 import { AngularFireAuth } from 'angularfire2/auth'; 
 import { TrainingService } from '../training/training.service';
 import { UIService } from '../shared/ui.service';
+
+//To add state management
+import { Store } from '@ngrx/store';
+import * as fromApp from '../app.reducer';// to import anythin from the given file path
 
 @Injectable()
 export class AuthService {
@@ -18,36 +23,43 @@ export class AuthService {
     private router: Router,
     private afauth: AngularFireAuth, 
     private trainSer: TrainingService,
-    private uiSer: UIService
+    private uiSer: UIService,
+    private store: Store< {ui: fromApp.State} >
    ) { }  
   
   registerUser(authdata: AuthData)
   {
-    this.uiSer.loadingStateChange.next(true);
+    //this.uiSer.loadingStateChange.next(true);
+    this.store.dispatch({ type: 'START_LOADING' });
     this.afauth
     .auth.createUserWithEmailAndPassword(
       authdata.email, 
       authdata.password
     ).then(res => { 
-        console.log(res);
-        this.uiSer.loadingStateChange.next(false);
+        //console.log(res);
+        //this.uiSer.loadingStateChange.next(false);
+        this.store.dispatch({ type: 'STOP_LOADING' });
       })
       .catch( err=> {
-        this.uiSer.loadingStateChange.next(false);
+        //this.uiSer.loadingStateChange.next(false);
+        this.store.dispatch({ type: 'STOP_LOADING' });
         this.uiSer.showSnackbar(err.message, 'Dismiss', 4000);
       });
   }
 
   login(authdata: AuthData )
   {
-    this.uiSer.loadingStateChange.next(true);
+    // this.uiSer.loadingStateChange.next(true);
+    this.store.dispatch({ type: 'START_LOADING' });
     this.afauth
     .auth.signInWithEmailAndPassword(authdata.email, authdata.password).then(res => {
       //console.log(res);
-      this.uiSer.loadingStateChange.next(false);
+      // this.uiSer.loadingStateChange.next(false);
+      this.store.dispatch({ type: 'STOP_LOADING' });
     })
     .catch( err=> {
-      this.uiSer.loadingStateChange.next(false);
+      // this.uiSer.loadingStateChange.next(false);
+      this.store.dispatch({ type: 'STOP_LOADING' });
       this.uiSer.showSnackbar(err.message, 'Dismiss', 4000);
     });
   }
